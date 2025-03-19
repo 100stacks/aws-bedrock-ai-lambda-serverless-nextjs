2d287222e76fd03bba98e9192aeb142d3f802ebc
100stacks <100stacks@users.noreply.github.com>
2025-03-19 17:07:57 -0400

    config: project configuration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Increase the maximum allowed size for API requests to handle document uploads
+  api: {
+    bodyParser: {
+      sizeLimit: "10mb",
+    },
+  },
+  // For handling AWS SDK in the client
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { "aws-crt": "aws-crt" }];
+    return config;
+  },
+};
 
 export default nextConfig;
